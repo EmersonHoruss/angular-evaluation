8e3bb17322dc677604ecb94cd54a3684b3bf3ac9
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2024-06-09 16:04:05 -0500

    feat: button component tested

--- src/app/shared/button/button.component.spec.ts
+++ src/app/shared/button/button.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ButtonComponent } from './button.component';
+import { By } from '@angular/platform-browser';
 
 describe('ButtonComponent', () => {
   let component: ButtonComponent;
@@ -8,10 +9,9 @@ describe('ButtonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ButtonComponent]
-    })
-    .compileComponents();
-    
+      imports: [ButtonComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,53 @@ describe('ButtonComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the button text from ng-content', () => {
+    const buttonText = 'Hi world';
+
+    const button = fixture.debugElement.query(By.css('button')).nativeElement;
+    button.textContent = buttonText;
+
+    expect(button.textContent).toContain(buttonText);
+  });
+
+  it('should apply custom sytles', () => {
+    const style: Record<string, string> = { backgroundColor: 'red' };
+    component.style = style;
+    fixture.detectChanges();
+
+    const button: HTMLButtonElement = fixture.debugElement.query(
+      By.css('button')
+    ).nativeElement;
+    const computedStyle = getComputedStyle(button);
+    expect(computedStyle.backgroundColor).toBe('red');
+  });
+
+  it('should apply custom css class', () => {
+    const cssClass = 'custom-class';
+    component.cssClass = cssClass;
+    fixture.detectChanges();
+
+    const button = fixture.debugElement.query(By.css('button')).nativeElement;
+    expect(button.classList).toContain(cssClass);
+  });
+
+  it('should be disabled when the disabled input is true', () => {
+    component.disabled = true;
+    fixture.detectChanges();
+
+    const button = fixture.debugElement.query(By.css('button')).nativeElement;
+    expect(button.disabled).toBeTruthy();
+  });
+
+  it('should emit click event when clicked', () => {
+    const mockFn = jest.fn();
+    component.clicked.emit = mockFn;
+
+    const button = fixture.debugElement.query(By.css('button')).nativeElement;
+    button.click();
+
+    expect(mockFn).toHaveBeenCalled();
   });
 });
