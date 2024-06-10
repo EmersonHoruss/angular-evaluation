be3de4794a2eb423db9e6f14188fa9f9fb95e13f
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2024-06-09 20:22:35 -0500

    feat: unauthenticated service logic integrated in component

--- src/app/doable/ui/unauthenticated/unauthenticated.component.ts
+++ src/app/doable/ui/unauthenticated/unauthenticated.component.ts
@@ -1,10 +1,18 @@
-import { Component, ViewEncapsulation, computed, signal } from '@angular/core';
+import {
+  Component,
+  ViewEncapsulation,
+  computed,
+  inject,
+  signal,
+} from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AuthStateService } from '../../data-access/state/auth-state.service';
 
 @Component({
   selector: 'app-unauthenticated',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ReactiveFormsModule],
   template: `
     <div class="wrapper">
       <div class="tabs">
@@ -21,23 +29,29 @@ import { ButtonComponent } from '../../../shared/button/button.component';
           Signup
         </app-button>
       </div>
-      <form>
+      <form [formGroup]="form">
         <div class="form-field">
           <label class="form-field__label">Email</label>
           <input
             class="form-field__main-content"
             placeholder="user@example.com"
+            formControlName="email"
           />
         </div>
 
         <div class="form-field">
           <label class="form-field__label">Password</label>
-          <input class="form-field__main-content" type="password" />
+          <input
+            class="form-field__main-content"
+            type="password"
+            formControlName="password"
+          />
         </div>
 
         <app-button cssClass="button-default w-full" (clicked)="handleAction()">
           {{ action() }}
         </app-button>
+        <div class="error">{{ service.error()?.description }}</div>
       </form>
     </div>
   `,
@@ -52,9 +66,28 @@ export class UnauthenticatedComponent {
   cssWhenSignupEnabled = computed(() =>
     this.tab() === 'signup' ? 'button-doable__active' : ''
   );
+
+  form!: FormGroup;
+
+  fb = inject(FormBuilder);
+  service = inject(AuthStateService);
+  constructor() {
+    this.form = this.fb.group({
+      email: [''],
+      password: [''],
+    });
+  }
+
   handleTab(tab: string) {
     this.tab.set(tab);
   }
 
-  handleAction() {}
+  handleAction() {
+    if (this.tab() === 'login') {
+      this.service.login$.next(this.form.value);
+    }
+    if (this.tab() === 'signup') {
+      this.service.signup$.next(this.form.value);
+    }
+  }
 }
